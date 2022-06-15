fb9a89a44e4cd5bf1593e7825df9e0f9659f24e3
Adam Dusty <adam.dusty27@gmail.com>
2022-06-15 07:11:31 -0700

    Rendering paddles.. sort of

--- samples/pong/main.cpp
+++ samples/pong/main.cpp
@@ -14,6 +14,10 @@ struct Input {
     std::vector<SDL_KeyCode> keys;
 };
 
+struct DrawInfo {
+    std::array<int32_t, 2> quad;
+};
+
 auto movement_system(entt::registry &reg, float dt) -> void {
     auto view = reg.view<Input, Position>();
 
@@ -48,9 +52,33 @@ auto input_system(entt::registry &reg, int *quit) -> void {
     }
 }
 
+auto render_system(entt::registry &reg, SDL_Renderer *rend) -> void {
+    auto view = reg.view<DrawInfo, Position>();
+
+    SDL_RenderClear(rend);
+
+    SDL_Rect rect{};
+
+    for(auto [e, d, p]: view.each()) {
+        SDL_SetRenderDrawColor(rend, 255, 255, 255, 255);
+
+        rect.w = d.quad.at(0);
+        rect.h = d.quad.at(1);
+        rect.x = p.x;
+        rect.y = p.y;
+        SDL_RenderFillRect(rend, &rect);
+        SDL_SetRenderDrawColor(rend, 0, 0, 0, 255);
+    }
+
+    SDL_RenderPresent(rend);
+}
+
 auto create_paddle(entt::registry &reg, float x, float y) -> entt::entity {
     auto ent = reg.create();
     reg.emplace<Position>(ent, x, y);
+    reg.emplace<DrawInfo>(ent, 10, 40);
+
+    return ent;
 }
 
 auto main() -> int {
@@ -59,13 +87,18 @@ auto main() -> int {
     auto dt = std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0).count();
 
     auto reg = entt::registry{};
-    auto inp = reg.create();
-    reg.emplace<Position>(inp);
-    reg.emplace<Input>(inp);
 
     SDL_Init(SDL_INIT_VIDEO);
     auto window =
         SDL_CreateWindow("title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1920, 1080, SDL_WINDOW_RESIZABLE);
+
+    int w, h;
+    SDL_GetWindowSize(window, &w, &h);
+
+    create_paddle(reg, w * 0.1, h / 2);
+    create_paddle(reg, w * 0.9, h / 2);
+
+    auto renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
     int quit = 0;
 
@@ -75,10 +108,12 @@ auto main() -> int {
 
         input_system(reg, &quit);
         movement_system(reg, dt);
+        render_system(reg, renderer);
 
         t1 = std::chrono::high_resolution_clock::now();
     }
 
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
 }
